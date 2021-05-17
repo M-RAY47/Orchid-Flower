217425d857ad7144c89e57a63b971d0c2084cf39
M.RAY <a.mray47@yahoo.fr>
2021-05-17 21:05:40 +0800

    Orchid Flower project

---
+++ helloworld.java
@@ -0,0 +1,12 @@
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        int a=8;
+        int b=6;
+        if(a>b){
+            int c =a+b;
+            System.out.println(c);
+        }
+    }
+    
+}
